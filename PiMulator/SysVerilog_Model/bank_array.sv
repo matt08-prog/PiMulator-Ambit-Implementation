a28ce66fa9d91e742aed7a6660309e9658b4a43a
matt08-prog <matthewroloff@gmail.com>
2026-04-20 11:50:49 -0600

    began work on AMBIT implementation

--- PiMulator/SysVerilog_Model/bank_array.sv
+++ PiMulator/SysVerilog_Model/bank_array.sv
@@ -2,7 +2,8 @@
 
 module bank_array #(
     parameter WIDTH = 8, 
-    parameter DEPTH = 2048,
+    parameter DEPTH = 1024, // From SIMDRAM paper: Using a typical subarray size of 1024 rows,
+                                // SIMDRAM splits the row addressing into 1006 D-group rows, 2 C-group rows, and 16 B-group rows
     parameter COLWIDTH = 10, // Added to understand row boundaries
     parameter CHWIDTH = 5    // Added to understand row boundaries
 ) (
@@ -14,6 +15,9 @@ module bank_array #(
     input logic [WIDTH-1:0] i_data, 
     output logic [WIDTH-1:0] o_data 
 );
+    enum {C0 = 1006, C1 = 1007} CONTROL_GROUP;
+    enum {T0 = 1008, T1, T2, T3, DCC0, n_DCC0, DCC1, n_DCC1, n_DCC0_T0, n_DCC1_T1, T2_T3, T0_T3, T0_T1_T2, T1_T2_T3, DCC0_T1_T2, DCC1_T0_T3} BITWISE_GROUP;
+
     (* ram_style = "block" *) logic [WIDTH-1:0] memory_array [0:DEPTH-1];
     
     `ifndef SYNTHESIS
